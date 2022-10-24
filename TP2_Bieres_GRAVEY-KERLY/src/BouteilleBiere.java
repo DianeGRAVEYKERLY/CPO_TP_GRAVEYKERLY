98395a5aa7dbc10389abe48a6fac8d56a4719309
dia12 <dia12@LAPTOP-1CA5JJ5U.education.lakanal.epf.fr>
2022-10-24 12:41:02 +0200

    création de la classe bouteillebiere , attribut de unebiereet action lireetiquette

---
+++ TP2_Bieres_GRAVEY-KERLY/src/BouteilleBiere.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dia12
+ */
+public class BouteilleBiere {
+    String nom;
+    float degreAlcool;
+    String brasserie;
+    boolean ouverte;
+}
+Public void lireEtiquette() {
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +
+ " degres) \nBrasserie : " + brasserie ) ;
+
+}
