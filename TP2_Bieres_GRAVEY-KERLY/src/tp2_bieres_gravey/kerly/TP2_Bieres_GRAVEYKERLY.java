b02051f68a4b2dd5f554c9c6a49a0bf5e3eeafac
dia12 <dia12@LAPTOP-1CA5JJ5U.education.lakanal.epf.fr>
2022-10-24 22:22:48 +0200

    partie 1: J'ai crée une classe bouteillebiere (qui ne marche pas), 4 objets (les beires), les attributs de bouteille biere, avec instruction de lireetiquette et de decapsuler

---
+++ TP2_Bieres_GRAVEY-KERLY/src/tp2_bieres_gravey/kerly/TP2_Bieres_GRAVEYKERLY.java
@@ -0,0 +1,37 @@
+6R/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_gravey.kerly;
+
+/**
+ *
+ * @author dia12
+ */
+public class TP2_Bieres_GRAVEYKERLY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7 ,"Dubuisson") ;
+    uneBiere.lireEtiquettte;
+    
+    
+    BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6
+,"Abbaye de Leffe") ;
+    autreBiere.lireEtiquette;
+    BouteilleBiere uneTroisiemeBiere = new BouteilleBiere("86",
+8.0 ,"Babar") ;
+    uneTroisiemeBiere.lireEtiquette;
+    BouteilleBiere uneQuatriemeBiere = new BouteilleBiere("Heineken",
+12.0 ,"Petit Comptoir") ;
+    uneQuatriemeBiere.lireEtiquette;
+    BouteilleBiere uneCinquiemeBiere = new BouteilleBiere("Desperados",
+14.0 ,"Dubuisson") ;
+    uneCinquiemeBiere.lireEtiquette;
+    
+    }
+}
