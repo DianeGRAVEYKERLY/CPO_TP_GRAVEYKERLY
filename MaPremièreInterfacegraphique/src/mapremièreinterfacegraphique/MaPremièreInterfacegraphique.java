bbbe19b77414f5af3db82d946ec5e993d945bde1
dia12 <dia12@LAPTOP-1CA5JJ5U.education.lakanal.epf.fr>
2022-11-07 11:18:41 +0100

    mise en forme du design, création 3 labels 3 textfields et deux boutons. Si cliquer bouton alors le prenom le nom et l'age s'affiche , sur l'autre bouton ils s'effacent.

--- MaPremièreInterfacegraphique/src/mapremièreinterfacegraphique/MaPremièreInterfacegraphique.java
+++ MaPremièreInterfacegraphique/src/mapremièreinterfacegraphique/MaPremièreInterfacegraphique.java
@@ -13,8 +13,5 @@ public class MaPremièreInterfacegraphique {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+  
 }
