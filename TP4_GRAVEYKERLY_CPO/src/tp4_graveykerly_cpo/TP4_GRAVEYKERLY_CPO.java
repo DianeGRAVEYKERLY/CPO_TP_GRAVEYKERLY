e3b5ec752107277e8a21db214319a8233ef91a9a
dia12 <dia12@LAPTOP-1CA5JJ5U.education.lakanal.epf.fr>
2022-11-08 15:50:04 +0100

    création de deux objets epees, de la classe baton et epee en heritage de la classe arme avec les attributs,

---
+++ TP4_GRAVEYKERLY_CPO/src/tp4_graveykerly_cpo/TP4_GRAVEYKERLY_CPO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp4_graveykerly_cpo;
+
+/**
+ *
+ * @author dia12
+ */
+public class TP4_GRAVEYKERLY_CPO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Epee premiereEpee= new Epee("excalibur", 7, 5);
+        Epee deuxiemeEpee= new Epee ("durandal",5,6);
+        
+        // TODO code application logic here
+    }
+    
+}
