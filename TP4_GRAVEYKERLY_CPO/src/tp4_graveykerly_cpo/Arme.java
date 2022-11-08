3b9baf3825a681482208c4081bb1582232927935
dia12 <dia12@LAPTOP-1CA5JJ5U.education.lakanal.epf.fr>
2022-11-08 15:28:53 +0100

    création du projet TP3 et création de la classe arme

---
+++ TP4_GRAVEYKERLY_CPO/src/tp4_graveykerly_cpo/Arme.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp4_graveykerly_cpo;
+
+/**
+ *
+ * @author dia12
+ */
+public class Arme {
+String prenom;
+int niveauAttaque ;
+
+public Arme (String n, int i) {
+prenom = n;
+niveauAttaque=i;
+
+
+}
+    
+}
