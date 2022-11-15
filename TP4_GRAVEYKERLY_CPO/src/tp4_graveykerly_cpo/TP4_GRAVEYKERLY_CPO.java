62636b2ce11d15c89c1394f316f7ee59c7ff6943
dia12 <dia12@LAPTOP-1CA5JJ5U.education.lakanal.epf.fr>
2022-11-15 12:44:22 +0100

    création de deux objets guerriers et deux objets magiciens, avec affichage tableau avec magiciens et guerriers

--- TP4_GRAVEYKERLY_CPO/src/tp4_graveykerly_cpo/TP4_GRAVEYKERLY_CPO.java
+++ TP4_GRAVEYKERLY_CPO/src/tp4_graveykerly_cpo/TP4_GRAVEYKERLY_CPO.java
@@ -4,15 +4,17 @@
  */
 package tp4_graveykerly_cpo;
 
-import Armes.Epee;
+import Armes.*;
 import java.util.ArrayList;
 
 /**
  *
  * @author dia12
  */
-public class TP4_GRAVEYKERLY_CPO {
 
+
+
+public class TP4_GRAVEYKERLY_CPO {
     /**
      * @param args the command line arguments
      */
@@ -22,20 +24,35 @@ public class TP4_GRAVEYKERLY_CPO {
         Baton premierBaton= new Baton ("chêne",4,5);
         Baton deuxiemeBaton = new Baton ("charme", 5,6);
        
-        ArrayList<Arme> Tab = new ArrayList<String> ();
+        ArrayList<Arme> Tab = new ArrayList<Arme> ();
         Tab.add(premiereEpee);
         Tab.add(deuxiemeEpee);
         Tab.add(premierBaton);
         Tab.add(deuxiemeBaton);
         
         System.out.print("["+Tab.get(0));
-        for (int i= 1; i<Tab.size());i++){
+        for (int i= 1; i<Tab.size();i++){
         System.out.print(","+Tab.get(i));
         
     }
         
                 
     System.out.print("]");    // TODO code application logic here
-    }
     
-}
+    Magicien PremierMagicien= new Magicien ("Gandalf",65,true);
+    Magicien DeuxiemeMagicien= new Magicien("Garcimore",44,false);
+    guerrier PremierGuerrier = new guerrier("Conan", 78, false);
+    guerrier DeuxiemeGuerrier = new guerrier("Lannister", 45, false );
+    
+     ArrayList<personnage> Tableau = new ArrayList<personnage> ();
+        Tab.add(PremierMagicien);
+        Tab.add(DeuxiemeMagicien);
+        Tab.add(PremierGuerrier);
+        Tab.add(DeuxiemeGuerrier);
+        
+        System.out.print("["+Tableau.get(0));
+        for (int i= 1; i<Tableau.size();i++){
+        System.out.print(","+Tableau.get(i));
+        }
+        
+}}
